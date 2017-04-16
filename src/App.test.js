e16dc6629570d09841aa7e29fd3dccc52e00a43b
pxai <pello_altadill@cuatrovientos.org>
2017-04-16 09:56:32 +0200

    Updated unit test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+var author = 'Pello Altadill';
+var headings = ['Id','Who','When','Description'];
+var data = [{
+    "id" : "1",
+    "who": "2",
+    "when": "2017-04-10",
+    "description": "Installed React"
+}];
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App author={author} headings={headings} data={data} />, div);
 });
